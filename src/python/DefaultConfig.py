23dacf8c63cf9121003328cbc318a2a3d881a71c
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-09-09 15:53:53 +0000

    Default config file to fix ticket 95
    From: Hassen Riahi <Hassen.Riahi@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/AsyncStageout/trunk@8891 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DefaultConfig.py
+++ src/python/DefaultConfig.py
@@ -3,17 +3,17 @@ from WMCore.Configuration import Configuration
 
 config = Configuration()
 
-# This component has all the configuration of CherryPy
-config.component_('AsyncTransfer')
+config.section_("General")
+config.General.workDir = "/tmp"
 
+config.component_("AsyncTransfer")
+config.AsyncTransfer.namespace = "AsyncTransfer.AsyncTransfer"
+config.AsyncTransfer.componentDir  = config.General.workDir + "/AsyncTransfer"
+config.AsyncTransfer.logLevel = "DEBUG"
+config.AsyncTransfer.pollInterval = 2
 config.AsyncTransfer.couch_instance = 'http://admin:password@localhost:5984'
-
-config.AsyncTransfer.couch_database = 'crab/asynctransfer'
-
-config.AsyncTransfer.log_level = logging.DEBUG
-
+config.AsyncTransfer.couch_database = 'crabserver/asynctransfer'
 config.AsyncTransfer.max_files_per_transfer = 1000
-
 config.AsyncTransfer.pool_size = 2
-
+config.AsyncTransfer.log_level = logging.DEBUG
 config.AsyncTransfer.ftscp = '../test/python/AsyncTransfer/ftscp_fake'
