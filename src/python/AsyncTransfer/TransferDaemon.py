e901c006480e6a1b536bb620dce2541f5aba7f51
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2011-03-03 13:19:33 +0000

    Write unit test, fixes #754
    From: Hassen Riahi <Hassen.Riahi@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/AsyncStageout/trunk@11763 4525493e-7705-40b1-a816-d608a930855b

--- src/python/AsyncTransfer/TransferDaemon.py
+++ src/python/AsyncTransfer/TransferDaemon.py
@@ -29,7 +29,7 @@ import logging
 def ftscp(user, tfc_map, config):
     worker = TransferWorker(user, tfc_map, config)
     return worker()
-    
+
 
 class TransferDaemon(BaseWorkerThread):
     def __init__(self, config):
@@ -38,37 +38,37 @@ class TransferDaemon(BaseWorkerThread):
         #logging.basicConfig(format = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',datefmt = '%m-%d %H:%M')
         #self.logger = logging.getLogger()
         # self.logger is set up by the BaseWorkerThread, we just set it's level
-        
+
         self.config = config.AsyncTransfer
         self.logger.setLevel(self.config.log_level)
         self.logger.debug('Configuration loaded')
-        
+
         server = CouchServer(self.config.couch_instance)
         self.db = server.connectDatabase(self.config.files_database)
         self.logger.debug('Connected to CouchDB')
 
         self.phedex = PhEDEx(responseType='xml')
-        
+
     # Over riding setup() is optional, and not needed here
-        
-    def algorithm(self, parameters):
+
+    def algorithm(self, parameters = None):
         """
-        
+
         1. Get a list of users with files to transfer from the couchdb instance
         2. For each user get a suitably sized input for ftscp (call to a list)
         3. Submit the ftscp to a subprocess
-        
+
         """
         users = self.active_users(self.db)
-        
+
         sites = self.active_sites()
         self.logger.info('%s active sites' % len(sites))
         self.logger.debug('Active sites are: %s' % sites)
-        
+
         site_tfc_map = {}
         for site in sites:
             site_tfc_map[site] = self.get_tfc_rules(site)
-            
+
         self.logger.debug('kicking off pool')
         pool = Pool(processes=self.config.pool_size)
         r = [pool.apply_async(ftscp, (u, site_tfc_map, self.config)) for u in users]
@@ -77,49 +77,49 @@ class TransferDaemon(BaseWorkerThread):
         for result in r:
             if result.ready():
                 self.logger.info(result.get(1))
-            
+
     def active_users(self, db):
         """
-        Query a view for users with files to transfer. Get this from the 
+        Query a view for users with files to transfer. Get this from the
         following view:
             ftscp?group=true&group_level=1
         """
         query = {'group': True, 'group_level':1}
         users = db.loadView('AsyncTransfer', 'ftscp', query)
-        
+
         active_users = []
         if len(users['rows']) <= self.config.pool_size:
             active_users = users['rows']
         else:
             #TODO: have a plugin algorithm here...
-            active_users = random.sample(users['rows'], self.config.pool_size) 
-        
+            active_users = random.sample(users['rows'], self.config.pool_size)
+
         def keys_map(dict):
             return dict['key'][0]
-        
+
         return map(keys_map, active_users)
-    
+
     def active_sites(self):
         """
         Get a list of all sites involved in transfers.
         """
         query = {'group': True}
         sites = self.db.loadView('AsyncTransfer', 'sites', query)
-        
+
         def keys_map(dict):
             return dict['key']
-        
+
         return map(keys_map, sites['rows'])
-    
+
     def get_tfc_rules(self, site):
         """
         Get the TFC regexp for a given site.
         """
         self.phedex.getNodeTFC(site)
         tfc_file = self.phedex.cacheFileName('tfc', inputdata={'node': site})
-         
+
         return readTFC(tfc_file)
-        
+
 if __name__ == '__main__':
     """
     Something temporary while I write the unit tests...
@@ -127,7 +127,7 @@ if __name__ == '__main__':
     import sys
     cfg_file = sys.path[0].replace('AsyncTransfer', 'DefaultConfig.py')
     config = loadConfigurationFile(cfg_file)
-    
+
     d = TransferDaemon(config)
     #while True:
     d.algorithm([])
---
+++ test/python/AsyncTransfer/AsyncTransfer_t.py
@@ -0,0 +1,370 @@
+#!/usr/bin/env python
+
+"""
+AsyncTransfer test
+"""
+
+import os
+import logging
+import unittest
+import time
+
+from WMCore.Agent.Configuration import loadConfigurationFile, Configuration
+
+from WMQuality.TestInit   import TestInit
+
+from AsyncTransfer.TransferDaemon import TransferDaemon
+from AsyncTransfer.LFNSourceDuplicator import LFNSourceDuplicator
+
+from WMCore.Database.CMSCouch import CouchServer
+
+class AsyncTransferTest(unittest.TestCase):
+    """
+    TestCase for TestAsyncTransfer module
+    """
+
+    def setUp(self):
+        """
+        setup for test.
+        """
+        self.testInit = TestInit(__file__)
+        self.testInit.setLogging()
+        self.testInit.setDatabaseConnection()
+        self.testInit.setSchema(customModules = ["WMCore.MsgService", "WMCore.ThreadPool"],
+                                useDefault = False)
+
+
+        self.docsInFilesDB = []
+        self.docsInDBSource = []
+
+        self.testDir = self.testInit.generateWorkDir(deleteOnDestruction = False)
+
+        self.config = self.getConfig()
+
+        # Connect to files db
+        server = CouchServer(self.config.AsyncTransfer.couch_instance)
+        self.db = server.connectDatabase(self.config.AsyncTransfer.files_database)
+        print('Connected to async couchDB')
+
+        # Connect to couchDB source
+        server = CouchServer(self.config.AsyncTransfer.data_source)
+        self.dbSource = server.connectDatabase(self.config.AsyncTransfer.jsm_db)
+        print('Connected to CouchDB source')
+
+        return
+
+    def tearDown(self):
+        """
+        Database deletion
+        """
+
+        self.testInit.clearDatabase(modules = ["WMCore.MsgService", "WMCore.ThreadPool"])
+        self.testInit.delWorkDir()
+
+        # Remove test docs in couchDB
+        for doc in self.docsInFilesDB:
+           self.DeleteTestDocinFilesDB(doc)
+
+        for doc in self.docsInDBSource:
+           self.DeleteTestDocinDBSource(doc)
+
+        return
+
+
+    def getConfig(self):
+        """
+        _createConfig_
+
+        General config file
+        """
+        config = Configuration()
+
+        #First the general stuff
+        config.section_("General")
+        config.General.workDir = os.getenv("TESTDIR", os.getcwd())
+
+        #Now the CoreDatabase information
+        config.section_("CoreDatabase")
+        config.CoreDatabase.connectUrl = os.getenv("DATABASE")
+        config.CoreDatabase.socket     = os.getenv("DBSOCK")
+
+        config.component_("AsyncTransfer")
+        config.AsyncTransfer.couch_instance = 'http://user:pass@crab.pg.infn.it:5984'
+        config.AsyncTransfer.files_database = 'asynctransfer_unitest'
+        config.AsyncTransfer.data_source = 'http://user:pass@crab.pg.infn.it:5984'
+        config.AsyncTransfer.jsm_db = 'wmagent_test'
+        config.AsyncTransfer.log_level = logging.DEBUG
+        config.AsyncTransfer.pluginName = "JSM"
+        config.AsyncTransfer.pluginDir = "AsyncTransfer.Plugins"
+        config.AsyncTransfer.max_files_per_transfer = 10
+        config.AsyncTransfer.pool_size = 3
+        config.AsyncTransfer.max_retry = 3
+        config.AsyncTransfer.serviceCert = os.getenv('X509_USER_PROXY')
+        config.AsyncTransfer.map_FTSserver = { 'PT' : 'https://fts.pic.es:8443/glite-data-transfer-fts/services/FileTransfer' , 'ES' : 'https://fts.pic.es:8443/glite-data-transfer-fts/services/FileTransfer' , 'IT' : 'https://fts.cr.cnaf.infn.it:8443/glite-data-transfer-fts/services/FileTransfer' , 'UK' : 'https://lcgfts.gridpp.rl.ac.uk:8443/glite-data-transfer-fts/services/FileTransfer' , 'FR' : 'https://cclcgftsprod.in2p3.fr:8443/glite-data-transfer-fts/services/FileTransfer' , 'CH' : 'https://prod-fts-ws.cern.ch:8443/glite-data-transfer-fts/services/FileTransfer' , 'DE' : 'https://fts-fzk.gridka.de:8443/glite-data-transfer-fts/services/FileTransfer' , 'TW' : 'https://w-fts.grid.sinica.edu.tw:8443/glite-data-transfer-fts/services/FileTransfer' , 'US' : 'https://cmsfts1.fnal.gov:8443/glite-data-transfer-fts/services/FileTransfer' , 'defaultServer' : 'https://fts.cr.cnaf.infn.it:8443/glite-data-transfer-fts/services/FileTransfer'}
+
+        config.AsyncTransfer.logDir                = os.path.join(self.testDir, 'logs')
+        config.AsyncTransfer.componentDir          = os.getcwd()
+
+        return config
+
+
+    def createTestDocinFilesDB(self):
+        """
+        Creates a test document in files_db
+
+        """
+        doc = {}
+
+        doc['_id'] = "/this/is/a/lfnA"
+        doc['dn'] = "/C=IT/O=INFN/OU=Personal Certificate/L=Perugia/CN=Hassen Riahi"
+        doc['workflow'] ='someWorkflow'
+        doc['jobid'] = '1'
+        doc['retry_count'] = []
+        doc['source'] = 'T2_IT_Bari'
+        doc['destination'] = "T2_IT_Pisa"
+        doc['user'] = 'riahi'
+        doc['state'] = 'new'
+        doc['start_time'] = str(time.time())
+        doc['dbSource_update'] = str(time.time())
+
+
+        self.db.queue(doc, True)
+        self.db.commit()
+
+        print('new doc added')
+
+        self.docsInFilesDB.append( doc['_id'] )
+
+        return
+
+    def DeleteTestDocinFilesDB(self, doc):
+        """
+        Remove the test documents in files_db
+        """
+        document = self.db.document( doc )
+        self.db.queueDelete(document)
+        self.db.commit()
+
+        print('deleted %s' %doc)
+
+        return
+
+
+    def createTestDocinDBSource(self):
+        """
+        Creates a JSM document
+        """
+
+        doc = {\
+   "_id": "12345",\
+   "timestamp": time.time(),\
+   "jobid": 7,\
+   "retrycount": 0,\
+   "fwjr": {\
+       "task": "/CmsRunAnalysis-22_121713/Analysis",\
+       "steps": {\
+           "logArch1": {\
+               "status": 0,\
+               "logs": {\
+               },\
+               "stop": 1290425610,\
+               "site": {\
+               },\
+               "input": {\
+               },\
+               "errors": [\
+               ],\
+               "parameters": {\
+               },\
+               "analysis": {\
+               },\
+               "start": 1290425601,\
+               "cleanup": {\
+               },\
+               "output": {\
+                   "logArchive": [\
+                       {\
+                           "runs": {\
+                           },\
+"lfn": "/store/user/riahi/lfnB",\
+"pfn": "srm://this/is/a/pfnB",\
+                           "module_label": "logArchive",\
+                           "location": "gridse3.pg.infn.it",\
+                           "events": 0,\
+                           "size": 0\
+                       }\
+                   ]\
+               }\
+           },\
+           "cmsRun1": {\
+               "status": 0,\
+               "logs": {\
+               },\
+               "stop": 1290425590,\
+               "site": {\
+               },\
+               "input": {\
+                   "source": [\
+                       {\
+                           "runs": {\
+                               "1": [\
+                                   39,\
+                                   60,\
+                                   73,\
+                                   78,\
+                                   80,\
+                                   112\
+                               ]\
+                           },\
+                           "input_source_class": "PoolSource",\
+                           "input_type": "primaryFiles",\
+                           "lfn": "/store/user/riahi/lfnB",\
+                           "pfn": "file:/this/is/a/pfnB",\
+                           "module_label": "source",\
+                           "guid": "D005BB56-CA2B-DF11-BA08-0030487C60AE",\
+                           "events": 600\
+                       }\
+                   ]\
+               },\
+               "errors": [\
+               ],\
+               "parameters": {\
+               },\
+               "analysis": {\
+               },\
+               "start": 1290425561,\
+               "cleanup": {\
+               },\
+               "output": {\
+                   "output": [\
+                       {\
+                           "branch_hash": "8dbc25d29c96c171aa2700e3c3249274",\
+                           "user_dn": "/C=IT/O=INFN/OU=Personal Certificate/L=Perugia/CN=Hassen Riahi",\
+                           "lfn": "/store/user/riahi/lfnB",\
+                           "dataset": {\
+                               "applicationName": "cmsRun",\
+                               "applicationVersion": "CMSSW_3_6_1_patch7",\
+                               "dataTier": "USER",\
+                           },\
+"InputPFN": "/home/cmsint/globus-tmp.fermi11.13080.0/https_3a_2f_2fcert-rb-01.cnaf.infn.it_3a9000_2frYxxZQTCljEAG6Q6-QgmhQ/job/WMTaskSpace/cmsRun1/output.root",\
+                           "checksums": {\
+                               "adler32": "fc729f97",\
+                               "cksum": "2529793973"\
+                           },\
+                           "guid": "5CD0D341-2CF6-DF11-9A92-0030487DA364",\
+                           "size": 16621775,\
+                           "location": "gridse3.pg.infn.it",\
+                           "async_dest": "T2_IT_Pisa",\
+                           "events": 600,\
+                           "ouput_module_class": "PoolOutputModule",\
+                           "pfn": "/this/is/a/pfn",\
+                           "catalog": "",\
+                           "module_label": "output",\
+                           "input": [\
+"/store/mc/JobRobot/RelValProdTTbar/GEN-SIM-DIGI-RECO/MC_3XY_V24_JobRobot-v1/0000/D005BB56-CA2B-DF11-BA08-0030487C60AE.root"\
+                           ],\
+                           "StageOutCommand": "srmv2-lcg",\
+                           "runs": {\
+                               "1": [\
+                                   39,\
+                                   60,\
+                                   73,\
+                                   78,\
+                                   80,\
+                                   112\
+                               ]\
+                           },\
+"OutputPFN": "srm://gridse3.pg.infn.it:8443/srm/managerv2?SFN=/cms//store/user/grandi/22_121713/0000/5CD0D341-2CF6-DF11-9A92-0030487DA364.root"\
+                       }\
+                   ]\
+               }\
+           },\
+           "stageOut1": {\
+               "status": 0,\
+               "logs": {\
+               },\
+               "stop": 1290425601,\
+               "site": {\
+               },\
+               "input": {\
+               },\
+               "errors": [\
+               ],\
+               "parameters": {\
+               },\
+               "analysis": {\
+               },\
+               "start": 1290425591,\
+               "cleanup": {\
+               },\
+               "output": {\
+               }\
+           }\
+       }\
+   },\
+   "type": "fwjr"\
+}
+
+
+
+        self.dbSource.queue(doc)
+        self.dbSource.commit()
+        print('Duplication done')
+
+        self.docsInDBSource.append( doc['_id'] )
+        self.docsInFilesDB.append( '/store/user/riahi/lfnB' )
+
+        return
+
+
+    def DeleteTestDocinDBSource(self, doc):
+        """
+        Deletes test docs from DB source
+
+        """
+        document = self.dbSource.document( doc )
+        self.dbSource.queueDelete( document )
+        self.dbSource.commit()
+
+        print('deleted %s' %doc)
+        return
+
+
+    def testA_BasicFunctionTest(self):
+        """
+        _BasicFunctionTest_
+
+        Tests the components, by seeing if they can process a simple document
+        """
+        self.createTestDocinFilesDB()
+
+        Transfer = TransferDaemon(config = self.config)
+        Transfer.algorithm( )
+
+        return
+
+
+    def testB_DuplicateDataFromJSM_BasicFunctionTest(self):
+        """
+        _SpeedTest_
+
+        Tests the components: gets data from DB source and duplicate
+        it in files_db and see if the component can process it.
+        """
+
+        self.createTestDocinDBSource()
+
+        LFNDuplicator = LFNSourceDuplicator(config = self.config)
+        LFNDuplicator.algorithm( )
+
+        time.sleep(10)
+
+        Transfer_1 = TransferDaemon(config = self.config)
+        Transfer_1.algorithm( )
+
+        return
+
+
+if __name__ == '__main__':
+    unittest.main()
+
