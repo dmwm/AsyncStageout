88742795f42d791661cf840500911dccbe023167
spiga <spiga@4525493e-7705-40b1-a816-d608a930855b>
2011-11-28 14:33:43 +0000

    fixes #2899 patch-async-riahi/
    From: spiga <spiga@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/AsyncStageout/branches/AsyncStageout_0_0_X@14656 4525493e-7705-40b1-a816-d608a930855b

--- src/couchapp/monitor/views/filesCount/map.js
+++ src/couchapp/monitor/views/filesCount/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-	if (doc.state) {
+	if (doc.lfn) {
 		emit([doc.lfn], {"state": doc.state, "destination": doc.destination, "source": doc.source, "user": doc.user, "task": doc.task});
 	}
 }
