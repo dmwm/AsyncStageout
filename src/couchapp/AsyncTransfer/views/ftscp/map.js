d8b9f449cbbbcbb9641517adbe52a1de046a3a1e
spiga <spiga@4525493e-7705-40b1-a816-d608a930855b>
2012-02-24 08:35:50 +0000

    Fix ftscp view and queries, fixes #3295
    From: spiga <spiga@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/AsyncStageout/trunk@15296 4525493e-7705-40b1-a816-d608a930855b

--- src/couchapp/AsyncTransfer/views/ftscp/map.js
+++ src/couchapp/AsyncTransfer/views/ftscp/map.js
@@ -1,5 +1,5 @@
 function(doc) {
         if (doc.state != 'failed' && doc.state != 'done' && doc.lfn) {
-		emit([doc.user, doc.destination, doc.source, doc.dn, doc.group, doc.role], doc.lfn);
+		emit([doc.user, doc.group, doc.role, doc.destination, doc.source, doc.dn], doc.lfn);
 	}
 }
