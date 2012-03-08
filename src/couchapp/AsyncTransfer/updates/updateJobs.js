4670984168fdf087fa5f8f10a4e3e4ccd520c527
spiga <spiga@4525493e-7705-40b1-a816-d608a930855b>
2012-03-08 22:27:29 +0000

    patch-series-riahi/ fixes #3335
    From: spiga <spiga@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/AsyncStageout/branches/AsyncStageout_0_0_X@15364 4525493e-7705-40b1-a816-d608a930855b

--- src/couchapp/AsyncTransfer/updates/updateJobs.js
+++ src/couchapp/AsyncTransfer/updates/updateJobs.js
@@ -1,6 +1,11 @@
 function (doc,req) {
-        doc.retry_count = req.query.retry_count;
-        doc.end_time = req.query.end_time;
+        if (req.query.state != 'done'){
+	        doc.retry_count.push(req.query.retry);
+                if (req.query.state == 'failed'){
+			doc.end_time = req.query.end_time;
+	        }
+	}
+        doc.last_update = parseInt(req.query.last_update)
         doc.state = req.query.state;
         return [doc, "OK"];
 }
