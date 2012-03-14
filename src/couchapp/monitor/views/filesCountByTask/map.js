b5f6d530fad01d033d24a23b2ee545b958aaffef
spiga <spiga@4525493e-7705-40b1-a816-d608a930855b>
2012-03-14 11:58:07 +0000

    patch-async-riahi/ fixes #2732
    From: spiga <spiga@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/AsyncStageout/trunk@15423 4525493e-7705-40b1-a816-d608a930855b

--- src/couchapp/monitor/views/filesCountByTask/map.js
+++ src/couchapp/monitor/views/filesCountByTask/map.js
@@ -1,5 +1,11 @@
 function(doc) {
 	if(doc.task){
-		emit([doc.task], {"state": doc.state, "user": doc.user, "destination": doc.destination, "source": doc.source, "id": doc.lfn});
-	}
+                var task_name = doc.task ;
+                var task = doc.task.split("/");
+                if (task.length > 1){
+                        task_name = task[1]
+                }
+
+                emit([task_name], {"state": doc.state, "user": doc.user, "destination": doc.destination, "source": doc.source, "id": doc.lfn});
+        }
 }
