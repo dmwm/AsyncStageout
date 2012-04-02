a291599f1fc202b2dad4c00caf0ef79cf8e30120
spiga <spiga@4525493e-7705-40b1-a816-d608a930855b>
2012-04-02 15:38:18 +0000

    From 72d68821fb157ce7cf03f85a1a10e80ab4291e73 Mon Sep 17 00:00:00 2001 Subject: [PATCH] Adding view for getoutput - fixes #3461
    From: mcinquil <mcinquil@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/AsyncStageout/trunk@15581 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/couchapp/UserMonitoring/views/FilesByWorkflow/map.js
@@ -0,0 +1,5 @@
+function(doc) {
+    if (doc.type == 'aso_file') {
+        emit([doc.workflow, doc.timestamp], {'lfn': doc.lfn, 'location': doc.location, 'checksum': doc.checksum, 'size': doc.size, 'jobid': doc.jobid})
+    }
+}
