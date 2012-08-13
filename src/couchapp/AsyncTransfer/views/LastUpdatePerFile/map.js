64eb7b7bfdf726266b4ce06fc7dce799edc8c9e9
Hassen Riahi <Hassen.Riahi@cern.ch>
2012-08-13 14:10:35 +0200

    Propagate transfers errors to the user_monitoring

--- src/couchapp/AsyncTransfer/views/LastUpdatePerFile/map.js
+++ src/couchapp/AsyncTransfer/views/LastUpdatePerFile/map.js
@@ -1,12 +1,12 @@
 function complete_job(doc, req) {
-        if ( doc['state'] != 'done' ) {
-                return false;
+        if ( doc['state'] != 'done' || doc['state'] != 'failed' ) {
+                return true;
         }
-        return true;
+        return false;
 }
 
 function(doc) {
 	if(doc.lfn && complete_job(doc)){
-		emit(doc.last_update, {"lfn": doc.lfn, "workflow": doc.workflow, "location": doc.destination, "checksum": doc.checksums, "jobid": doc.jobid, "retry_count": doc.retry_count.length+1, "size": doc.size});
+		emit(doc.last_update, {"lfn": doc.lfn, "state": doc.state, "errors": doc.failure_reason, "workflow": doc.workflow, "location": doc.destination, "checksum": doc.checksums, "jobid": doc.jobid, "retry_count": doc.retry_count.length+1, "size": doc.size});
 	}
 }
