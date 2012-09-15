f2d77254792fc0be40997a5dcb4562f53124f1cd
Hassen Riahi <Hassen.Riahi@cern.ch>
2012-09-15 19:16:21 +0200

    Fix views to get correctly outputs.

--- src/couchapp/UserMonitoring/views/FilesByWorkflow/map.js
+++ src/couchapp/UserMonitoring/views/FilesByWorkflow/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-    if (doc.type == 'aso_file') {
+    if ( doc.type == 'aso_file'  && doc.state == 'done' ) {
         emit([doc.workflow, doc.timestamp], {'lfn': doc.lfn, 'location': doc.location, 'checksum': doc.checksum, 'size': doc.size, 'jobid': doc.jobid})
     }
 }
