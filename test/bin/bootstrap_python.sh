6f34aba9316d6528d4dc519e3a53981af310330f
Brian Bockelman <bbockelm@cse.unl.edu>
2014-02-18 10:36:25 -0600

    Tune publication test cases to cover MC.

--- test/bin/bootstrap_python.sh
+++ test/bin/bootstrap_python.sh
@@ -12,5 +12,6 @@ export PYTHONPATH=$CRAB3_CHECKOUT/CAFTaskWorker/src/python:$CRAB3_CHECKOUT/WMCor
 export PYTHONPATH=$CRAB3_CHECKOUT/CAFTaskWorker/test/python:$PYTHONPATH
 export PYTHONPATH=$CRAB3_CHECKOUT/AsyncStageout/src/python:$PYTHONPATH
 export PYTHONPATH=$CRAB3_CHECKOUT/DBS/Client/src/python:$CRAB3_CHECKOUT/DBS/PycurlClient/src/python:$PYTHONPATH
+export LD_LIBRARY_PATH=$CRAB3_CHECKOUT/AsyncStageout/src/python:$LD_LIBRARY_PATH
 
 exec python2.6 "$@"
