1f89e88aaf073d92c587c1368cda415008afaf40
Justas Balčas <juztas@gmail.com>
2014-03-05 11:39:24 +0100

    Fix monitoring view

--- src/couchapp/monitor/vendor/couchapp/_attachments/loader.js
+++ src/couchapp/monitor/vendor/couchapp/_attachments/loader.js
@@ -1,4 +1,3 @@
-
 function couchapp_load(scripts) {
   for (var i=0; i < scripts.length; i++) {
     document.write('<script src="'+scripts[i]+'"><\/script>')
@@ -6,11 +5,7 @@ function couchapp_load(scripts) {
   document.write('<link href="vendor\/protovis\/tipsy\/tipsy.css" type="text\/css" rel="stylesheet"\/>')
 };
 
-couchapp_load([
-  "/_utils/script/sha1.js",
-  "/_utils/script/json2.js",
-  "/_utils/script/jquery.js",
-  "/_utils/script/jquery.couch.js",
+var couchapp_files = [
   "vendor/couchapp/jquery.couch.app.js",
   "vendor/couchapp/jquery.couch.app.util.js",
   "vendor/couchapp/jquery.mustache.js",
@@ -19,4 +14,23 @@ couchapp_load([
   "vendor/protovis/tipsy/jquery.tipsy.js",
   "vendor/protovis/tipsy/tipsy.js",
   "vendor/plots/plots.js"
-]);
+]
+var check_files = [
+  "/_utils/script/sha1.js",
+  "/_utils/script/json2.js",
+  "/_utils/script/jquery.js",
+  "/_utils/script/jquery.couch.js"
+]
+
+var parser = document.createElement('a');
+parser.href = document.URL;
+var append = "";
+if (!parser.port)
+{
+append = "/couchdb";
+}
+for (var i = 0; i < check_files.length; i++) {
+    couchapp_files.push(append + check_files[i]);
+}
+
+couchapp_load(couchapp_files);
