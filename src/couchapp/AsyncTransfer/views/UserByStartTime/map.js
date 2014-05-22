2d44fc5619a1a4dd131fbc4147dce2bae9a49ecd
Hassen Riahi <Hassen.Riahi@cern.ch>
2014-05-22 10:20:35 +0200

    Minor fix + remove old couchapp

--- src/couchapp/AsyncTransfer/views/UserByStartTime/map.js
+++ src/couchapp/AsyncTransfer/views/UserByStartTime/map.js
@@ -1,5 +1,5 @@
 function(doc) {
         if (doc.state != 'failed' && doc.state != 'done' && doc.lfn) {
-                emit([doc.user, doc.group, doc.role, doc.start_time], 1);
+                emit([doc.user, doc.group, doc.role, doc.start_time.split('.')[0]], 1);
         }
 }
