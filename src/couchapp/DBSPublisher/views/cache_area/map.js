945f0c60171baf40b8b877246210134b0a9849bf
Diego <dciangot@cern.ch>
2015-05-14 11:32:16 +0200

    Patches for #4348 and #4347

---
+++ src/couchapp/DBSPublisher/views/cache_area/map.js
@@ -0,0 +1,9 @@
+function(doc) {
+
+        if (doc.publication_state != 'published' && doc.publication_state != 'publication_failed' && doc.state == 'done' && doc.lfn && doc.dbs_url && doc.rest_host && doc.rest_uri && doc.publish == 1) {
+                                        emit( doc.workflow, [doc.rest_host, doc.rest_uri]);
+                        }
+                }
+        }
+}
+
