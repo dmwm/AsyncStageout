4ccb752852666239df9ff80b1a4d903916ada187
Andres Tanasijczuk <andres.jorge.tanasijczuk@cern.ch>
2015-07-16 13:48:19 +0200

    Change publish view to emit source and destination LFNs.

--- src/couchapp/DBSPublisher/views/publish/map.js
+++ src/couchapp/DBSPublisher/views/publish/map.js
@@ -1,7 +1,6 @@
 function(doc) {
 
-        if (doc.publication_state != 'published' && doc.publication_state != 'publication_failed' && doc.state == 'done' && doc.lfn && doc.dbs_url) {
-                var lfn = doc.lfn.replace('store', 'store/temp')
+        if (doc.publication_state != 'published' && doc.publication_state != 'publication_failed' && doc.state == 'done' && doc.source_lfn && doc.destination_lfn && doc.dbs_url) {
                 var publish = 0
                 if (doc.publish == 1) {
                         publish = doc.publish
@@ -9,7 +8,7 @@ function(doc) {
                 if (doc.type != 'log'){
                         if (publish == 1){
                                 var dbs_url = doc.dbs_url
-                                        emit([doc.user, doc.group, doc.role, doc.workflow], [doc.destination, lfn, doc.inputdataset, dbs_url, doc.end_time.split('.')[0]]);
+                                        emit([doc.user, doc.group, doc.role, doc.workflow], [doc.destination, doc.source_lfn, doc.destination_lfn, doc.inputdataset, dbs_url, doc.end_time.split('.')[0]]);
                         }
                 }
         }
