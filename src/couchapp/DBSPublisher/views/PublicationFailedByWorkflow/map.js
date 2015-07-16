e9e22dec5dead3ee8003a9e5773d31008dad0cd0
Hassen Riahi <Hassen.Riahi@cern.ch>
2015-07-16 16:57:35 +0200

    Expose publication failures via API.

--- src/couchapp/DBSPublisher/views/PublicationFailedByWorkflow/map.js
+++ src/couchapp/DBSPublisher/views/PublicationFailedByWorkflow/map.js
@@ -1,7 +1,7 @@
 function(doc) {
 	if(doc.workflow){
-		if(doc.publication_state=='publication_failed'){
-			emit(doc.workflow, doc._id);
+		if(doc.publication_state=='publication_failed' && doc.publication_failure_reason){
+			emit([doc.workflow,doc.publication_failure_reason], _sum);
 		}
 	}
 }
