4038e1a09c3b0fe7c78b72a009bcd90dc8a28fbc
Diego <dciangot@cern.ch>
2016-07-19 13:00:25 +0200

    applying Stefano's comments

--- bin/DashboardNotifier.py
+++ bin/DashboardNotifier.py
@@ -16,7 +16,6 @@ def produce(file_path, logging, conn):
     message = json.load(json_data)
     logging.debug("Producing...%s" % message)
     try:
-        logging.debug("Sending %s" % message)
         messageDict = json.dumps(message)
         conn.send(messageDict, destination=authParams['MSG_QUEUE'] )
     except Exception as ex:
@@ -24,24 +23,21 @@ def produce(file_path, logging, conn):
         msg += str(ex)
         msg += str(traceback.format_exc())
         logging.debug(msg)
-        raise 
 
 logging.basicConfig(filename='/data/srv/asyncstageout/current/config/log', level=logging.DEBUG)
 amq_auth_file = "/data/srv/asyncstageout/current/config/asyncstageout/amq_auth_file.json"
 opened = False
 
-while not opened:
-    try:
-        f = open(amq_auth_file)
-        authParams = json.loads(f.read())
-        opened = True
-        f.close()
-    except Exception as ex:
-        msg = "Error loading auth params"
-        msg += str(ex)
-        msg += str(traceback.format_exc())
-        logging.debug(msg)
-        pass
+try:
+    f = open(amq_auth_file)
+    authParams = json.loads(f.read())
+    opened = True
+    f.close()
+except Exception as ex:
+    msg = "Error loading auth params"
+    msg += str(ex)
+    msg += str(traceback.format_exc())
+    logging.debug(msg)
 
 try:
     host = [(authParams['MSG_HOST'], authParams['MSG_PORT'])]
@@ -53,18 +49,15 @@ except Exception as ex:
     msg += str(ex)
     msg += str(traceback.format_exc())
     logging.debug(msg)
-    raise
 
 for dashboard_file in os.listdir("/tmp"):
     logging.debug(dashboard_file)
     if os.path.basename(dashboard_file).endswith('json'):
-        logging.debug(dashboard_file)
         file_path = '/tmp/' + dashboard_file
         logging.debug(file_path)
         p = Process(target=produce, args=(file_path, logging, conn))
         p.start()
         p.join()
-        logging.debug("ok")
         logging.debug("Removing file at %s" % datetime.datetime.now())
         os.unlink( '/tmp/' + dashboard_file )
 
