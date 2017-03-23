9ddaa81cff4c6d6d9cf7006223c8fdca7ca30c0f
dciangot <dciangot@cern.ch>
2017-03-23 14:19:06 +0100

    Kill transfer component

--- src/python/AsyncStageOut/RetryManagerDaemon.py
+++ src/python/AsyncStageOut/RetryManagerDaemon.py
@@ -12,14 +12,24 @@ import urllib
 import logging
 import datetime
 import traceback
+import json
+from datetime import timedelta
+from Queue import Queue
+from threading import Thread
+import fts3.rest.client.easy as fts3
+import re
 
 from WMCore.WMFactory import WMFactory
 from WMCore.WMException import WMException
 from WMCore.Database.CMSCouch import CouchServer
 
+from AsyncStageOut import getProxy
+from AsyncStageOut import getDNFromUserName
 from AsyncStageOut.BaseDaemon import BaseDaemon
+from AsyncStageOut import getHashLfn
 from RESTInteractions import HTTPRequests
-from ServerUtilities import encodeRequest
+from ServerUtilities import  generateTaskName,\
+        PUBLICATIONDB_STATUSES, encodeRequest, oracleOutputMapping
 
 __all__ = []
 
@@ -114,11 +124,181 @@ class RetryManagerDaemon(BaseDaemon):
             try:
                 results = self.oracleDB.post(self.config.oracleFileTrans,
                                              data=encodeRequest(fileDoc))
-                logging.info("Retried files in cooloff: %s" % str(results))
             except Exception:
                 self.logger.exception("Failed to get retry transfers in oracleDB: %s")
+                return
+            logging.info("Retried files in cooloff: %s,\n now getting transfers to kill" % str(results))
+
+            fileDoc = dict()
+            fileDoc['asoworker'] = self.config.asoworker
+            fileDoc['subresource'] = 'getTransfersToKill'
+            fileDoc['grouping'] = 0
+            try:
+                results = self.oracleDB.get(self.config.oracleFileTrans,
+                                            data=encodeRequest(fileDoc))
+                result = oracleOutputMapping(results)
+            except Exception as ex:
+                self.logger.error("Failed to get killed transfers \
+                                   from oracleDB: %s" % ex)
+                return
+
+            usersToKill = list(set([(x['username'], x['user_group'], x['user_role']) for x in result]))
+
+            self.logger.debug("Users with transfers to kill: %s" % usersToKill)
+            transfers = Queue()
+
+            for i in range(self.config.kill_threads):
+                worker = Thread(target=self.killThread, args=(i, transfers,))
+                worker.setDaemon(True)
+                worker.start()
+
+            for user in usersToKill:
+                user_trans = [x for x in result if (x['username'], x['user_group'], x['user_role']) == user]
+                self.logger.info("Inserting %s transfers of user %s in the killing queue" % (len(user_trans), user))
+                transfers.put(user_trans)
+
+            transfers.join()
+            self.logger.info("Transfers killed.")
         else:
             self.doRetries()
+
+    def killThread(self, thread_id, transfers):
+        """This is the worker thread function for kill command.
+        """
+        while True:
+            transfer_list = transfers.get()
+            self.logger.info("Starting thread %s" % (thread_id))
+            user = transfer_list[0]['username']
+            group = transfer_list[0]['user_group']
+            role = transfer_list[0]['user_role']
+
+            uiSetupScript = getattr(self.config, 'UISetupScript', None)
+
+            self.logger.debug("Trying to get DN for %s %s %s %s" % (user, self.logger, self.config.opsProxy, self.config.opsProxy))
+            try:
+                userDN = getDNFromUserName(user, self.logger, ckey=self.config.opsProxy, cert=self.config.opsProxy)
+            except Exception as ex:
+                msg = "Error retrieving the user DN"
+                msg += str(ex)
+                msg += str(traceback.format_exc())
+                self.logger.error(msg)
+                continue
+            if not userDN:
+                transfers.task_done()
+                time.sleep(1)
+                continue
+            self.logger.debug("user DN: %s" % userDN)
+
+            try:
+                defaultDelegation = {'logger': self.logger,
+                                     'credServerPath': self.config.credentialDir,
+                                     'myProxySvr': 'myproxy.cern.ch',
+                                     'min_time_left': getattr(self.config, 'minTimeLeft', 36000),
+                                     'serverDN': self.config.serverDN,
+                                     'uisource': uiSetupScript,
+                                     'cleanEnvironment': getattr(self.config, 'cleanEnvironment', False)}
+                if hasattr(self.config, "cache_area"):
+                    cache_area = self.config.cache_area
+                    defaultDelegation['myproxyAccount'] = re.compile('https?://([^/]*)/.*').findall(cache_area)[0]
+            except IndexError:
+                self.logger.error('MyproxyAccount parameter cannot be retrieved from %s . ' % self.config.cache_area)
+                transfers.task_done()
+                time.sleep(1)
+                continue
+            if getattr(self.config, 'serviceCert', None):
+                defaultDelegation['server_cert'] = self.config.serviceCert
+            if getattr(self.config, 'serviceKey', None):
+                defaultDelegation['server_key'] = self.config.serviceKey
+            try:
+                defaultDelegation['userDN'] = userDN
+                defaultDelegation['group'] = group if group else ''
+                defaultDelegation['role'] = role if group else ''
+                self.logger.debug('delegation: %s' % defaultDelegation)
+                valid_proxy, user_proxy = getProxy(defaultDelegation, self.logger)
+            except Exception as ex:
+                msg = "Error getting the user proxy"
+                msg += str(ex)
+                msg += str(traceback.format_exc())
+                self.logger.error(msg)
+                transfers.task_done()
+                time.sleep(1)
+                continue
+
+            # TODO: take server from db, right now, take only the first of the list and assuming it valid for all
+            try:
+                # TODO: debug u added during info upload. To be fixed soon! For now worked around
+                fts_server = transfer_list[0]['fts_instance'].split('u')[1]
+                self.logger.info("Delegating proxy to %s" % fts_server)
+                context = fts3.Context(fts_server, user_proxy, user_proxy, verify=True)
+                self.logger.debug(fts3.delegate(context, lifetime=timedelta(hours=48), force=False))
+
+                self.logger.info("Proxy delegated. Grouping files by jobId")
+                jobs = {}
+                for fileToKill in transfer_list:
+                    # TODO: debug u added during info upload. To be fixed soon! For now worked around
+                    jid = str(fileToKill['fts_id']).split('u')[1]
+                    if jid not in jobs:
+                        jobs[jid] = []
+                    jobs[jid].append(fileToKill)
+
+                self.logger.info("Found %s jobIds", len(jobs.keys()))
+                self.logger.debug("jobIds: %s", jobs.keys)
+
+                # list for files killed or failed to
+                killed = []
+                too_late = []
+
+                for ftsJobId, files in jobs.iteritems():
+                    self.logger.info("Cancelling tranfers in %s" % ftsJobId)
+
+                    ref_lfns = [str(x['destination_lfn'].split('/store/')[1]) for x in files]
+                    source_lfns = [x['source_lfn'] for x in files]
+
+                    job_list = fts3.get_job_status(context, ftsJobId, list_files=True)
+                    tx = job_list['files']
+
+                    # TODO: this workaround is needed to get FTS file id, we may want to add a column in the db?
+                    idListToKill = [x['file_id'] for x in tx
+                                    if x['dest_surl'].split('/cms/store/')[1] in ref_lfns]
+
+                    # needed for the state update
+                    lfnListToKill = [ref_lfns.index(str(x['dest_surl'].split('/cms/store/')[1])) for x in tx
+                                       if x['dest_surl'].split('/cms/store/')[1] in ref_lfns]
+
+                    self.logger.debug("List of ids to cancel for job %s: %s" % (ftsJobId, idListToKill))
+                    res = fts3.cancel(context, ftsJobId, idListToKill)
+                    self.logger.debug('Kill command result: %s' % json.dumps(res))
+
+                    if not isinstance(res, list):
+                        res = [res]
+
+                    # Verify if the kill command succeeded
+                    for k, kill_res in enumerate(res):
+                        indexToUpdate = lfnListToKill[k]
+                        if kill_res in ("FINISHEDDIRTY", "FINISHED", "FAILED"):
+                            self.logger.debug(source_lfns[indexToUpdate])
+                            too_late.append(getHashLfn(source_lfns[indexToUpdate]))
+                        else:
+                            killed.append(getHashLfn(source_lfns[indexToUpdate]))
+
+                # TODO: decide how to update status for too_late files
+                killed += too_late
+                self.logger.debug('Updating status of killed files: %s' % killed)
+
+                if len(killed) > 0:
+                    data = dict()
+                    data['asoworker'] = self.config.asoworker
+                    data['subresource'] = 'updateTransfers'
+                    data['list_of_ids'] = killed
+                    data['list_of_transfer_state'] = ["KILLED" for _ in killed]
+                    self.oracleDB.post(self.config.oracleFileTrans,
+                                       data=encodeRequest(data))
+                    self.logger.debug("Marked killed %s" % killed)
+            except:
+                # TODO: split and improve try/except
+                self.logger.exception('Kill command failed')
+
+            transfers.task_done()
 
     def processRetries(self, files):
         """
